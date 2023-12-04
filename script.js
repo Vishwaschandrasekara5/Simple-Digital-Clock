987c367b0db6545d1bae1709d16c532a98c7e2d4
Vishwa Sandun Chandrasekara <sandunv05@gmail.com>
2023-12-04 21:29:17 +0530

    First Commit
    Date : 2023/12/04

---
+++ script.js
@@ -0,0 +1,16 @@
+// setInterval(updateClock, 1000);
+
+// function updateclock(){
+
+//     var mydate = new Date();
+    
+//     var myhour = mydate.getHours();
+//     var mymin = mydate.getMinutes();
+//     var mysec = mydate.getSeconds();
+
+//     var mclock = myhour + ":" + mymin + ":" + mysec;
+
+//     document.getElementById("clock").innerHTML = mclock;
+
+
+// }
